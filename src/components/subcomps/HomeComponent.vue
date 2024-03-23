87835c072a39cb45f41257eb3b10c26d264cc7ac
charliecharles03 <cloud9stark@gmail.com>
2024-03-23 16:48:22 +0530

    profile added svg

--- src/components/subcomps/HomeComponent.vue
+++ src/components/subcomps/HomeComponent.vue
@@ -1,5 +1,11 @@
 <template>
-  <div><h4>User Tag palce</h4></div>
+  <div class="UserProfileClass">
+    <button @click="profileClicked()" class="UserProfileButton">
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+    </svg>
+    </button>
+  </div>
   <div>
     <header>
       <div class="container">
@@ -15,7 +21,7 @@
       </div>
     </section>
 
-    <h2>Songs Gallery</h2>
+    <h2>Recently Added Songs</h2>
     <SongGallery :songs="songs" />
 
     <section class="features">
@@ -63,8 +69,11 @@ export default {
     }
   },
   methods: {
-    onclick () {
+    onClick () {
       console.log('button clicked!')
+    },
+    profileClicked () {
+      console.log('profile clicked!')
     }
   }
 }
@@ -186,6 +195,17 @@ footer {
 
 footer p {
   font-size: 0.8em;
+}
+
+.UserProfileClass{
+  text-align: right;
+  padding: 10px;
+  font-size: 20px;
+  margin-bottom: 20px;
+}
+
+.UserProfileButton{
+  border-radius: 15px;
 }
 
 </style>
